945127db8c00b56f7888e94e8a57714c60763158
tomjames156 <tomjames156@gmail.com>
2025-05-19 19:06:16 +0100

    updated main activity removed warning

--- app/src/main/java/ooad/capstone/lockedin/MainActivity.java
+++ app/src/main/java/ooad/capstone/lockedin/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
